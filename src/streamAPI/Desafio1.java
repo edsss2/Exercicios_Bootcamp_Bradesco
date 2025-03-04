93eea8d948b9af997e67892ac94774a683d623ef
Edson Sanches <edsons@RM.GROUP>
2025-03-04 16:27:24 -0300

    Começando desafio de StramAPI

--- src/streamAPI/Desafio1.java
+++ src/streamAPI/Desafio1.java
@@ -1,5 +1,23 @@
 package streamAPI;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Desafio1 {
 
+//	Desafio 1 - Mostre a lista na ordem numérica:
+//		Crie um programa que utilize a Stream API para ordenar a lista de números em ordem crescente e a exiba no console.
+	
+	public static void main(String[] args) {
+	    
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+		
+		
+		
+	}
 }
