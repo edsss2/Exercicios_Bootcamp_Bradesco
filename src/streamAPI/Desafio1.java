4580995ace4e2411b75224bff58e125bf3c690fb
edsss2 <edsonsanchezveiga@gmail.com>
2025-03-05 08:58:39 -0300

    desafio 1 e 2 concluidos

--- src/streamAPI/Desafio1.java
+++ src/streamAPI/Desafio1.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,6 +18,11 @@ public class Desafio1 {
 	    
 		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 		
+		List<Integer> numerosOrdenados = numeros.stream().sorted().toList();
+			
+		numerosOrdenados.forEach(System.out::println);
+			
+
 		
 		
 	}
---
+++ src/streamAPI/Desafio2.java
@@ -0,0 +1,24 @@
+package streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Desafio2 {
+	
+	//Desafio 2 - Imprima a soma dos números pares da lista:
+	//Utilizando a Stream API, realize a soma dos números pares da lista e exiba o resultado no console.
+
+	public static void main(String[] args) {
+		
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+		
+		List<Integer> pares = numeros.stream()
+				.filter(n -> n % 2 ==0)
+				.toList();
+		
+		int resultado = pares.stream().reduce(0, Integer::sum);
+		
+		System.out.println(resultado);
+	}
+}
